b79c3f178117f1c69566f25c9cae0f98e321167e
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-02-16 16:49:49 +0400

    completed the task bubble_sort

--- tasks/hard/bubble_sort.py
+++ tasks/hard/bubble_sort.py
@@ -28,8 +28,21 @@
 """
 
 
+# def bubble_sort(array: list) -> list:
+#     for i in range(len(array) - 1):
+#         for j in range(len(array) - 1):
+#             if (array[j] > array[j + 1]):
+#                 temp = array[j]
+#                 array[j] = array[j + 1]
+#                 array[j + 1] = temp
+#     return array
+
 def bubble_sort(array: list) -> list:
-    return None
+    for i in range(len(array) - 1):
+        for j in range(len(array) - 1 - i):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
 
 
 if __name__ == '__main__':
